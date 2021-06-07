631a7e7403c6f6667e0587963cd5aab3daf2ce3a
Afonso Gloeden <contact@afonsogloeden.com>
2021-06-06 22:00:13 -0300

    feat: eslint
    ESLint v7.28.0

--- App.tsx
+++ App.tsx
@@ -2,6 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const backgroundColor = '#fff';
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor,
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
+
 export default function App() {
   return (
     <View style={styles.container}>
@@ -10,12 +21,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
